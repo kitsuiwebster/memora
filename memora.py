7bffba12e9f7847d1c64d3df923d501cf2bef567
kitsuiwebster <raphmartin1999@gmail.com>
2023-08-18 13:52:52 +0200

    feat: store variables in config.py

--- memora.py
+++ memora.py
@@ -1,6 +1,8 @@
 # @name: Memora
 # @author: kitsuiwebster
 
+from config import streak_of_10, streak_of_50, streak_of_100, streak_of_150, streak_of_200, ascii_100, ascii_1000, ascii_goodbye, ascii_memora
+
 import random
 import json
 from colorama import init, Fore
@@ -11,28 +13,12 @@ init()
 print(Fore.YELLOW + """
               
               Welcome to""")
-print("""
- __  __  ___  __  __   ___   ___    _   
-|  \/  || __||  \/  | / _ \ | _ \  /_\    
-| |\/| || _| | |\/| || (_) ||   / / _ \   
-|_|  |_||___||_|  |_| \___/ |_|_\/_/ \_\  
-
-""" + Fore.RESET)
+print(f"{ascii_memora}" + Fore.RESET)
 
 # This is the Goodbye message
 def print_goodbye_message():
-    print("""
-     _______  _______  _______  ______   _______  __   __  _______ 
-    |       ||       ||       ||      | |  _    ||  | |  ||       |
-    |    ___||   _   ||   _   ||  _    || |_|   ||  |_|  ||    ___|
-    |   | __ |  | |  ||  | |  || | |   ||       ||       ||   |___ 
-    |   ||  ||  |_|  ||  |_|  || |_|   ||  _   | |_     _||    ___|
-    |   |_| ||       ||       ||       || |_|   |  |   |  |   |___ 
-    |_______||_______||_______||______| |_______|  |___|  |_______|
-    """)
+    print(f"{ascii_goodbye}")
     print(Fore.YELLOW + "And see you soon :D")
-
-
 
 # This is the menu
 def choose_game_mode():
@@ -45,16 +31,7 @@ def choose_game_mode():
                 data = json.load(file)
             word_map = data
             print(Fore.YELLOW + "\nYou chose Centium:\n")
-            print(""" 
-             d888    .d8888b.   .d8888b.  
-            d8888   d88P  Y88b d88P  Y88b 
-              888   888    888 888    888 
-              888   888    888 888    888 
-              888   888    888 888    888 
-              888   888    888 888    888 
-              888   Y88b  d88P Y88b  d88P 
-            8888888  "Y8888P"   "Y8888P"  
-            """)
+            print(f"{ascii_100}")
             print("Let's start!" + Fore.RESET)
 
         elif user_choice == '2':
@@ -62,16 +39,7 @@ def choose_game_mode():
                 data = json.load(file)
             word_map = data
             print(Fore.YELLOW + "\nYou chose Millenium:\n")
-            print(""" 
-             d888    .d8888b.   .d8888b.   .d8888b.  
-            d8888   d88P  Y88b d88P  Y88b d88P  Y88b 
-              888   888    888 888    888 888    888 
-              888   888    888 888    888 888    888 
-              888   888    888 888    888 888    888 
-              888   888    888 888    888 888    888 
-              888   Y88b  d88P Y88b  d88P Y88b  d88P 
-            8888888  "Y8888P"   "Y8888P"   "Y8888P"
-            """)
+            print(f"{ascii_1000}")
             print("Let's start!" + Fore.RESET)
 
         elif user_choice == '3':
@@ -86,30 +54,6 @@ def choose_game_mode():
         return choose_game_mode() 
 
     return word_map
-
-
-
-# These are the congrats messages
-streak_of_10 = "CONGRATULATIONS ON HAVING 10 CORRECT ANSWERS IN A ROW, LET'S GOOOO!"
-streak_of_50 = "CONGRATULATIONS ON HAVING 50 CORRECT ANSWERS IN A ROW, LET'S GOOOO!"
-streak_of_100 = """CONGRATULATIONS, OH MASTER OF KNOWLEDGE AND WISDOM, FOR YOUR MAGNIFICENT FEAT OF ACING 100 QUESTIONS IN A ROW! YOUR BRAIN IS CLEARLY WIRED FOR GENIUS, YOUR NEURONS FIRED WITH PRECISION, AND YOUR INTELLECT SHINES BRIGHTER THAN A THOUSAND SUNS!
-
-YOU HAVE RISEN ABOVE THE MORTAL PLAIN OF TRIVIA, LEAVING YOUR OPPONENTS IN AWE AND DISBELIEF. YOUR VICTORIES ARE NOT MERE COINCIDENCES, BUT TESTIMONIES TO YOUR ASTOUNDING MENTAL FORTITUDE AND SHEER INTELLECTUAL MIGHT.
-
-THE GODS OF QUIZZES AND PUZZLES LOOK DOWN UPON YOU, AND EVEN THEY ARE LEFT ASTONISHED BY YOUR UNPARALLELED BRAINPOWER. YOUR BRAIN CELLS ARE THROWING A MASSIVE PARTY, POPPING CHAMPAGNE CORKS AND DOING THE CONGA IN CELEBRATION OF YOUR EPIC ACHIEVEMENT!
-
-YOU ARE THE ANSWER KEY TO THE ENIGMA OF TRIVIA, THE EINSTEIN OF QUIZZES, THE LEONARDO DA VINCI OF RIDDLES. YOUR MIND IS A SPECTACULAR LABYRINTH OF KNOWLEDGE, WHERE EVERY TURN LEADS TO THE RIGHT ANSWER, AND EVERY PATH UNVEILS A NEW LEVEL OF BRILLIANCE.
-
-SO, STAND TALL, OH CHAMPION OF THE MIND! BASK IN THE GLORY OF YOUR TRIUMPH, FOR YOU HAVE EARNED IT WITH YOUR UNWAVERING DEDICATION AND UNMATCHED CEREBRAL MIGHT. MAY YOUR STREAK OF SUCCESS CONTINUE, AND MAY YOUR NAME BE WHISPERED WITH REVERENCE AND ADMIRATION IN THE REALMS OF QUIZZES FOREVERMORE!
-
-IN YOUR HONOR, ALL DUCKS SHALL QUACK 100 TIMES, SQUIRRELS SHALL PAUSE THEIR NUT COLLECTION FOR 100 SECONDS, AND THE MOON WILL DO... WELL, IT CAN'T DO MUCH, BUT IT'S PROBABLY REALLY PROUD OF YOU TOO!
-
-MAY YOUR FUTURE BE FILLED WITH ENDLESS MORE STREAKS OF RIGHT ANSWERS AND MAY YOUR WRONG ANSWERS BE AS RARE AS A CHICKEN WHISTLING THE NATIONAL ANTHEM!
-
-CHEERS TO YOU, THE SULTAN OF SMARTS, THE RULER OF RIGHT ANSWERS! THIS IS YOUR DAY, SO ENJOY YOUR 100-CORRECT-ANSWER GLORY! WHOOO-HOOO !"""
-streak_of_150 = "CONGRATULATIONS ON HAVING 150 CORRECT ANSWERS IN A ROW, LET'S GOOOO!"
-streak_of_200 = "CONGRATS !!! YOU GOT 200 CORRECT ANSWERS IN A ROW, AAAAAAIGHT, YOU ROOOOOOCK!"
-
 
 word_map = choose_game_mode()
 score = 0
